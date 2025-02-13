fd2f908f6d42b3f26a1e6bc2eee7cd33b2093d7e
Ng Warren <90106059+ngwarren01@users.noreply.github.com>
2025-02-13 22:11:38 +0800

    Refactor installation script to remove merge conflict markers and clean up code

--- installation.py
+++ installation.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import subprocess
 import sys
 
@@ -6,13 +6,4 @@ def install_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 if __name__ == "__main__":
-=======
-import subprocess
-import sys
-
-def install_package(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-if __name__ == "__main__":
->>>>>>> 693751de77eeb8c60c1b63bac1bad7da24e87e1a
     install_package("pyautogui")
